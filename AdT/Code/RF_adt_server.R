b4142d9b981a50f307378c295011186ec49b8bd9
LegenDad <hj.jedonald@gmail.com>
2018-04-22 22:53:47 +0900

    rf edit

--- AdT/Code/RF_adt_server.R
+++ AdT/Code/RF_adt_server.R
@@ -2,7 +2,7 @@ rm(list=ls()); gc()
 #adt <- adt[sample(.N, 30e6), ]
 library(data.table)
 adt <- fread("../input/train.csv")
-adt <- adt[sample(.N, 10e5), ]
+adt <- adt[sample(.N, 10e6), ]
 library(lubridate)
 adt$click_hour <- hour(adt$click_time)
 adt$click_weekd <- wday(adt$click_time)
@@ -25,72 +25,28 @@ colnames(adt)[11:20] <- c("ip_hw", "ip_app", "ip_dev", "ip_os", "ip_ch",
 colnames(adt)
 
 library(caret)
-#args(createDataPartition)
-colnames(adt)
 adt_index<- createDataPartition(adt$is_attributed, p=0.7, list = F)
 y <- adt$is_attributed
-
 adtr <- adt %>% select(-ip, -click_time, -attributed_time)
-
 adte <- adtr[-adt_index,]
 adtr <- adtr[adt_index,]
-
-colnames(adte)
-colnames(adtr)
+colnames(adte) == colnames(adtr)
 table(adtr$is_attributed); table(adte$is_attributed)
-
-###### glm #####
-glm <- glm(is_attributed~., family = binomial, adtr)
-summary(glm)
-anova(glm, test = "Chisq")
-library(pscl)
-pR2(glm)
-format(3.510406e-01, scientific = F)
-pred_glm <- predict(glm, newdata = adte, type = "response")
-pred_glm2 <- ifelse(pred_glm > 0.5,1,0)
-sum(pred_glm2)
+rm(adt); gc()
 library(e1071)
-confusionMatrix(as.factor(pred_glm2), as.factor(y[-adt_index]))
 library(ROCR)
-pr <- prediction(pred_glm, y[-adt_index])
+library(randomForest)
+model.rf=randomForest(as.factor(is_attributed)~., adtr, 
+                      importance = T)
+model.rf
+importance(model.rf)
+pred_rf <- predict(model.rf, adte, type = "prob")[,2]
+pr <- prediction(pred_rf, adte$is_attributed)
 prf <- performance(pr, measure = "tpr", x.measure = "fpr")
 plot(prf)
 auc <- performance(pr, measure = "auc")
 auc <- auc@y.values[[1]]
 auc
-
-##### bayesglm #####
-library(arm)
-glm2 <- bayesglm(is_attributed~., family = binomial, adtr)
-summary(glm2)
-anova(glm2, test = "Chisq")
-pR2(glm2)
-format(3.510406e-01, scientific = F)
-pred_glm21 <- predict(glm2, newdata = adte, type = "response")
-pred_glm22 <- ifelse(pred_glm2 > 0.5,1,0)
-sum(pred_glm22)
-confusionMatrix(as.factor(pred_glm22), as.factor(y[-adt_index]))
-pr2 <- prediction(pred_glm21, y[-adt_index])
-prf2 <- performance(pr2, measure = "tpr", x.measure = "fpr")
-plot(prf2)
-auc2 <- performance(pr2, measure = "auc")
-auc2 <- auc2@y.values[[1]]
-auc2
-
-##### Random Forest #####
-library(randomForest)
-model.rf=randomForest(is_attributed~., adtr)
-model.rf
-plot(model.rf)
-pred_rf <- predict(model.rf, adte)
-pr3 <- prediction(pred_rf, adte$is_attributed)
-pr3 <- prediction(pred_rf, adte$is_attributed)
-prf3 <- performance(pr3, measure = "tpr", x.measure = "fpr")
-plot(prf3)
-
-auc3 <- performance(pr3, measure = "auc")
-auc3 <- auc3@y.values[[1]]
-auc3
 
 rm(adtr, adte); gc()
 
@@ -113,16 +69,9 @@ head(adte)
 colnames(adte)[9:18] <- c("ip_hw", "ip_app", "ip_dev", "ip_os", "ip_ch", 
                           "ip_cnt", "app_cnt", "dev_cnt", "os_cnt", "ch_cnt")
 adte <- adte %>% select(-ip, -click_time)
-
-######
-glmpred <- predict(glm, newdata = adte, type = "response")
+rfpred <- predict(model.rf, adte, type = "prob")[,2]
 sub <- fread("../input/sample_submission.csv")
-sub$is_attributed <- glmpred
-fwrite(sub, paste0("glm", auc, ".csv"))
-
-rfpred <- predict(model.rf, adte)
-sub <- fread("../input/sample_submission.csv")
-sub$is_attributed <- rfpred
-fwrite(sub, paste0("rf", auc3, ".csv"))
+sub$is_attributed <- round(rfpred, 6)
+fwrite(sub, paste0("rf", round(auc, 6), ".csv"))
 
 #####
