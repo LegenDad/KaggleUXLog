c6673e1704c206d443ca923a66db68326a31ad13
LegenDad <hj.jedonald@gmail.com>
2018-04-25 10:07:58 +0900

    kbig script

--- AdT/Code/adt_lgbm_kbig.R
+++ AdT/Code/adt_lgbm_kbig.R
@@ -1,39 +1,24 @@
 rm(list=ls()); gc()
 library(data.table)
-adt <- fread("../input/train_sample.csv")
 library(lubridate)
-adt$click_hour <- hour(adt$click_time)
-adt$click_weekd <- wday(adt$click_time)
 library(dplyr)
-colnames(adt)
-adt <- adt %>% add_count(ip, click_hour, click_weekd)
-adt <- adt %>% add_count(ip, app)
-adt <- adt %>% add_count(ip, device)
-adt <- adt %>% add_count(ip, os)
-adt <- adt %>% add_count(ip, channel)
-adt <- adt %>% add_count(ip)
-adt <- adt %>% add_count(app)
-adt <- adt %>% add_count(device)
-adt <- adt %>% add_count(os)
-adt <- adt %>% add_count(channel)
-head(adt)
-colnames(adt)[11:20] <- c("ip_hw", "ip_app", "ip_dev", "ip_os", "ip_ch", 
-                          "ip_cnt", "app_cnt", "dev_cnt", "os_cnt", "ch_cnt")
-colnames(adt)
-
-library(caret)
+adt <- readRDS("adt_50e6")
 set.seed(777)
+colnames(adt)
+library(caret)
+library(pryr)
 y <- adt$is_attributed
 adt_index <- createDataPartition(y, p = 0.7, list = F)
 tri <- createDataPartition(y[adt_index], p = 0.9, list = F)
 adtr <- adt %>% select(-ip, -click_time, -attributed_time, -is_attributed)
-
 library(lightgbm)
 dtrain <- lgb.Dataset(data = as.matrix(adtr[adt_index,][tri,]), 
                       label = y[adt_index][tri])
 dval <- lgb.Dataset(data = as.matrix(adtr[adt_index,][-tri,]), 
                     label = y[adt_index][-tri])
 dtest <- as.matrix(adtr[-adt_index,])
+rm(adt); gc()
+mem_used()
 params = list(objective = "binary", 
               metric = "auc", 
               learning_rate= 0.1, 
@@ -70,6 +55,16 @@ auc <- performance(pr, "auc")
 library(knitr)
 kable(lgb.importance(model_lgbm))
 lgb.plot.importance(lgb.importance(model_lgbm), top_n = 15)
-library(pryr)
 mem_used()
+rm(adt_index, dtest, dval, dtrain, adtr, cols, tri, y); gc()
+##### test data _ NOT RUN in Local #####
+adte <- readRDS("adte.RDS")
+adtest <- as.matrix(adte)
+realpred <- predict(model_lgbm, adtest, n = model_lgbm$best_iter)
+rm(adte); gc()
+sub <- fread("../input/sample_submission.csv")
+sub$is_attributed <- round(realpred, 6)
+fwrite(sub, paste0("adt", round(model_lgbm$best_score, 6), ".csv"))
 
+
+#####
