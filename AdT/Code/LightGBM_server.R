012d3323fec39cca5a999227532301589d65346d
LegenDad <hj.jedonald@gmail.com>
2018-05-01 01:39:42 +0900

    datatable

--- AdT/Code/LightGBM_server.R
+++ AdT/Code/LightGBM_server.R
@@ -4,49 +4,43 @@ adt <- fread("../input/train.csv")
 
 #Select Train Sizes
 set.seed(777)
-adt <- adt[sample(.N, 10e6), ]
+adt <- adt[sample(.N, 50e6), ]
 #adt <- adt[sample(.N, 20e6), ]
 #adt <- adt[sample(.N, 30e6), ]
 
 library(lubridate)
-adt$click_hour <- hour(adt$click_time)
-adt$click_weekd <- wday(adt$click_time)
-library(dplyr)
+adt[, click_hour := hour(adt$click_time)]
+adt[, click_weekd := wday(adt$click_time)]
+adt[, ip_hw := .N, by = list(ip, click_hour, click_weekd)]
+adt[, ip_app := .N, by = list(ip, app)]
+adt[, ip_dev := .N, by = list(ip, device)]
+adt[, ip_os := .N, by = list(ip, os)]
+adt[, ip_ch := .N, by = list(ip, channel)]
+adt[, ip_cnt := .N, by = ip]
+adt[, app_cnt := .N, by = app]
+adt[, dev_cnt := .N, by = device]
+adt[, os_cnt := .N, by = os]
+adt[, ch_cnt := .N, by = channel]
+adt[, clicker := .N, by = list(ip, device, os)]
+adt[, clicker_app := .N, by = list(ip, device, os, app)]
+adt[, clicker_N := seq(.N), by = list(ip, device, os)]
+adt[, clicker_app_N := seq(.N), by = list(ip, device, os, app)]
+dim(adt)
 colnames(adt)
-str(adt)
-adt <- adt %>% add_count(ip, click_hour, click_weekd) 
-adt <- adt %>% add_count(ip, app)
-adt <- adt %>% add_count(ip, device)
-adt <- adt %>% add_count(ip, os)
-adt <- adt %>% add_count(ip, channel)
-adt <- adt %>% add_count(ip)
-adt <- adt %>% add_count(app)
-adt <- adt %>% add_count(device)
-adt <- adt %>% add_count(os)
-adt <- adt %>% add_count(channel)
-head(adt)
-colnames(adt)[11:20] <- c("ip_hw", "ip_app", "ip_dev", "ip_os", "ip_ch", 
-                          "ip_cnt", "app_cnt", "dev_cnt", "os_cnt", "ch_cnt")
+
+te_hourG1 <- c(4, 14, 13, 10, 9, 5)
+te_hourG2 <- c(15, 11, 6)
+adt$h_div <- ifelse(adt$click_hour %in% te_hourG1, 1, 
+                    ifelse(adt$click_hour %in% te_hourG2, 3, 2))
 colnames(adt)
-#te_hourG1 <- c(4, 14, 13, 10, 9, 5)
-#te_hourG2 <- c(15, 11, 6)
-#adt$h_div <- ifelse(adt$click_hour %in% te_hourG1, 1, 
-#                    ifelse(adt$click_hour %in% te_hourG2, 3, 2))
-adt <- adt %>% add_count(ip, device, os)
-adt <- adt %>% add_count(ip, device, os, app)
-colnames(adt)[21:22] <- c("clicker", "clicker_app")
-colnames(adt)
-adt <- adt %>% group_by(ip, device, os) %>% mutate(clicker_N = 1:n())
-adt <- adt %>% group_by(ip, device, os, app) %>% mutate(clicke_app_N = 1:n())
-colnames(adt)
-adt <- as.data.table(adt)
+
 library(caret)
 set.seed(777)
 y <- adt$is_attributed
 adt_index <- createDataPartition(y, p = 0.7, list = F)
 tri <- createDataPartition(y[adt_index], p = 0.9, list = F)
 cat_f <- c("app", "device", "os", "channel", "click_hour")
-adtr <- adt %>% select(-ip, -click_time, -attributed_time, -is_attributed)
+adtr <- adt[, -c("ip", "click_time", "attributed_time", "is_attributed")]
 
 library(lightgbm)
 dtrain <- lgb.Dataset(data = as.matrix(adtr[adt_index,][tri,]), 
@@ -71,12 +65,8 @@ params = list(objective = "binary",
               min_split_gain= 0,
               scale_pos_weight=99.7)
 model_lgbm <- lgb.train(params, dtrain, valids = list(validation = dval), 
-                        nthread = 8, nrounds = 3000, verbose = 1, 
-                        early_stopping_rounds = 300, eval_freq = 10)
-#str(model_lgbm)
-#model_lgbm$record_evals
-#model_lgbm$record_evals[["validation"]]
-#model_lgbm$record_evals[["validation"]][["auc"]][["eval"]]
+                        nthread = 8, nrounds = 2000, verbose = 1, 
+                        early_stopping_rounds = 200, eval_freq = 10)
 model_lgbm$best_score
 model_lgbm$best_iter
 
@@ -99,39 +89,34 @@ rm(adt_index, dtest, dval, dtrain, adtr, tri, y); gc()
 
 ##### test data #####
 adte <- fread("../input/test.csv")
-adte <- adte %>% select(-click_id)
-adte$click_hour <- hour(adte$click_time)
-adte$click_weekd <- wday(adte$click_time)
-adte <- adte %>% add_count(ip, click_hour, click_weekd) 
-adte <- adte %>% add_count(ip, app)
-adte <- adte %>% add_count(ip, device)
-adte <- adte %>% add_count(ip, os)
-adte <- adte %>% add_count(ip, channel)
-adte <- adte %>% add_count(ip)
-adte <- adte %>% add_count(app)
-adte <- adte %>% add_count(device)
-adte <- adte %>% add_count(os)
-adte <- adte %>% add_count(channel)
-head(adte)
-colnames(adte)[9:18] <- c("ip_hw", "ip_app", "ip_dev", "ip_os", "ip_ch", 
-                          "ip_cnt", "app_cnt", "dev_cnt", "os_cnt", "ch_cnt")
-#adte$h_div <- ifelse(adte$click_hour %in% te_hourG1, 1, 
-#                    ifelse(adte$click_hour %in% te_hourG2, 3, 2))
-adte <- adte %>% add_count(ip, device, os)
-adte <- adte %>% add_count(ip, device, os, app)
-colnames(adte)[19:20] <- c("clicker", "clicker_app")
+adte[, click_hour := hour(adte$click_time)]
+adte[, click_weekd := wday(adte$click_time)]
+adte[, ip_hw := .N, by = list(ip, click_hour, click_weekd)]
+adte[, ip_app := .N, by = list(ip, app)]
+adte[, ip_dev := .N, by = list(ip, device)]
+adte[, ip_os := .N, by = list(ip, os)]
+adte[, ip_ch := .N, by = list(ip, channel)]
+adte[, ip_cnt := .N, by = ip]
+adte[, app_cnt := .N, by = app]
+adte[, dev_cnt := .N, by = device]
+adte[, os_cnt := .N, by = os]
+adte[, ch_cnt := .N, by = channel]
+adte[, clicker := .N, by = list(ip, device, os)]
+adte[, clicker_app := .N, by = list(ip, device, os, app)]
+adte[, clicker_N := seq(.N), by = list(ip, device, os)]
+adte[, clicker_app_N := seq(.N), by = list(ip, device, os, app)]
+dim(adte)
 colnames(adte)
-adte <- adte %>% group_by(ip, device, os) %>% mutate(clicker_N = 1:n())
-adte <- adte %>% group_by(ip, device, os, app) %>% mutate(clicke_app_N = 1:n())
+adte$h_div <- ifelse(adte$click_hour %in% te_hourG1, 1, 
+                    ifelse(adte$click_hour %in% te_hourG2, 3, 2))
 colnames(adte)
-
-adte <- as.data.table(adte)
-adte <- adte %>% select(-ip, -click_time)
+adte <- adte[, -c("click_id", "ip", "click_time")]
+colnames(adte)
 adte <- as.matrix(adte)
 realpred <- predict(model_lgbm, adte, n = model_lgbm$best_iter)
 sub <- fread("../input/sample_submission.csv")
 sub$is_attributed <- round(realpred, 6)
-fwrite(sub, paste0("AdT", round(model_lgbm$best_score, 6), ".csv"))
+fwrite(sub, paste0("AdT_", round(auc, 6), ".csv"))
 
 
 ##### END #####
