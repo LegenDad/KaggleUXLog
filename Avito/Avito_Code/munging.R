ef5175c1f20fd81dedf89d52c41f98ca1a148b32
LegenDad <hj.jedonald@gmail.com>
2018-06-08 01:43:39 +0900

    eee

--- Avito/Avito_Code/munging.R
+++ Avito/Avito_Code/munging.R
@@ -6,26 +6,28 @@ library(knitr)
 library(skimr)
 library(DT)
 avi <- read_csv("../input/train.csv")
-avtte <- read_csv("../input/test.csv")
+# avtte <- read_csv("../input/test.csv")
+# avtte_na <- sapply(avtte, function(x) sum(is.na(x)))
+# avtte_na[avtte_na > 0]
 avi_na <- sapply(avi, function (x) sum(is.na(x)))
 avi_na[avi_na>0]
-avtte_na <- sapply(avtte, function(x) sum(is.na(x)))
-avtte_na[avtte_na > 0]
 # p1_na, p2_na, p3_na, dsc_na, price_na, img_na, img_t1_na
 # test; dsc_na X
 
 avi$p1_na <- is.na(avi$param_1) %>% as.integer()
+avi$p2_na <- is.na(avi$param_2) %>% as.integer()
+avi$p3_na <- is.na(avi$param_3) %>% as.integer()
 table(avi$p1_na)
+table(avi$p2_na)
+table(avi$p3_na)
 
-skim(avi$image)
-# tr_te <- tr %>% 
-#   select(-deal_probability) %>% 
-#   bind_rows(te) %>% 
-#   mutate(no_img = is.na(image) %>% as.integer(),
-#          no_dsc = is.na(description) %>% as.integer(),
-#          no_p1 = is.na(param_1) %>% as.integer(), 
-#          no_p2 = is.na(param_2) %>% as.integer(), 
-#          no_p3 = is.na(param_3) %>% as.integer(),
+avi$dsc_na <- is.na(avi$description) %>% as.integer()
+avi$img_na <- is.na(avi$image) %>% as.integer()
+table(avi$dsc_na)
+table(avi$img_na)
+range(avi$image_top_1, na.rm = T)
+
+#   mutate(
 #          titl_len = str_length(title),
 #          desc_len = str_length(description),
 #          titl_cap = str_count(title, "[A-ZА-Я]"),
@@ -36,7 +38,7 @@ skim(avi$image)
 #          desc_dig = str_count(description, "[[:digit:]]"),
 #          user_type = as.factor(user_type) %>% as.integer(),
 #          category_name = category_name %>% factor() %>% as.integer(),
-#          parent_category_name = parent_category_name %>% factor() %>% as.integer(), 
+#          parent_category_name = parent_category_name %>% factor() %>% as.integer(),
 #          region = region %>% factor() %>% as.integer(),
 #          param_1 = param_1 %>% factor() %>% as.integer(),
 #          param_2 = param_2 %>% factor() %>% as.integer(),
@@ -46,10 +48,10 @@ skim(avi$image)
 #          price = log1p(price),
 #          txt = paste(title, description, sep = " "),
 #          mday = mday(activation_date),
-#          wday = wday(activation_date),         
-#          day = day(activation_date)) %>% 
-#   select(-item_id, -image, -title, -description, -activation_date) %>% 
-#   replace_na(list(image_top_1 = -1, price = -1, 
+#          wday = wday(activation_date),
+#          day = day(activation_date)) %>%
+#   select(-item_id, -image, -title, -description, -activation_date) %>%
+#   replace_na(list(image_top_1 = -1, price = -1,
 #                   param_1 = -1, param_2 = -1, param_3 = -1,
-#                   desc_len = 0, desc_cap = 0, desc_pun = 0, desc_dig = 0)) %T>% 
+#                   desc_len = 0, desc_cap = 0, desc_pun = 0, desc_dig = 0)) %T>%
 #   glimpse()
