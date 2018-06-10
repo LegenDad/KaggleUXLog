2d0ac8927b0354d8b1d18897585973726854c7d3
LegenDad <hj.jedonald@gmail.com>
2018-06-11 01:12:44 +0900

    add price

--- Avito/Avito_Code/munging.R
+++ Avito/Avito_Code/munging.R
@@ -81,42 +81,133 @@ pie(table(avi$param_3))
 # dsc_na, price_na, img_na, img_t1_na
 # test; dsc_na X
 avi$dsc_na <- is.na(avi$description) %>% as.integer()
-?str_length
-#          desc_len = str_length(description),
-#          desc_cap = str_count(description, "[A-ZА-Я]"),
-#          desc_pun = str_count(description, "[[:punct:]]"),
-#          desc_dig = str_count(description, "[[:digit:]]"),
-# replace_na desc_len = 0, desc_cap = 0, desc_pun = 0, desc_dig = 0
+
+avi$dsc_len = str_length(avi$description)
+range(avi$dsc_len, na.rm=T)
+avi$dsc_capE = str_count(avi$description, "[A-Z]")
+range(avi$dsc_capE)
+range(avi$dsc_capE, na.rm=T)
+avi$dsc_capR = str_count(avi$description, "[А-Я]")
+range(avi$dsc_capR, na.rm = T)
+avi$dsc_cap = str_count(avi$description, "[A-ZА-Я]")
+range(avi$dsc_cap, na.rm=T)
+avi <- avi %>% replace_na(list(dsc_capE=0, dsc_capR=0, dsc_cap=0))
+range(avi$dsc_cap)
+range(avi$dsc_capE)
+range(avi$dsc_capR)
+avi$dsc_pun = str_count(avi$description, "[[:punct:]]")
+avi$dsc_dig = str_count(avi$description, "[[:digit:]]")
+avi <- avi %>% replace_na(list(dsc_pun=0, dsc_dig=0))
 
 # price -------------------------------------------------------------------
+range(avi$price)
+range(avi$price, na.rm=T)
+# avi$price <- log(avi$price)
+avi$price <-log1p(avi$price)
+avi <- avi %>% replace_na(list(price = -1))
+
+# img ---------------------------------------------------------------------
 
 avi$img_na <- is.na(avi$image) %>% as.integer()
 table(avi$dsc_na)
 table(avi$img_na)
 range(avi$image_top_1, na.rm = T)
 
-#   mutate(
+# library(magrittr)
+# library(text2vec)
+# library(tokenizers)
+# library(stopwords)
+# library(xgboost)
+# library(Matrix)
+# library(stringr)
+# library(stringi)
+# library(forcats)
+
+#   mutate(no_img = is.na(image) %>% as.integer(),
 #          titl_len = str_length(title),
+#          titl_capE = str_count(title, "[A-Z]"),
+#          titl_capR = str_count(title, "[А-Я]"),
 #          titl_cap = str_count(title, "[A-ZА-Я]"),
 #          titl_pun = str_count(title, "[[:punct:]]"),
 #          titl_dig = str_count(title, "[[:digit:]]"),
-#          user_type = as.factor(user_type) %>% as.integer(),
-#          category_name = category_name %>% factor() %>% as.integer(),
-#          parent_category_name = parent_category_name %>% factor() %>% as.integer(),
-#          region = region %>% factor() %>% as.integer(),
-#          
-#          city = city %>% factor() %>% fct_lump(prop = 0.0003) %>% as.integer(),
-#          user_id = user_id %>% factor() %>% fct_lump(prop = 0.000025) %>% as.integer(),
-#          price = log1p(price),
+#          user_type = factor(user_type),
+#          category_name = factor(category_name) %>% as.integer(),
+#          parent_category_name = factor(parent_category_name) %>% as.integer(), 
+#          region = factor(region) %>% as.integer(),
+#          city =  factor(city) %>% fct_lump(prop = 0.0003) %>% as.integer(),
+#          user_id = factor(user_id) %>% fct_lump(prop = 0.000025) %>% as.integer(),
 #          txt = paste(title, description, sep = " "),
 #          mday = mday(activation_date),
-#          wday = wday(activation_date),
-#          day = day(activation_date)) %>%
-#   select(-item_id, -image, -title, -description, -activation_date) %>%
-#   replace_na(list(image_top_1 = -1, price = -1,
-#                   param_1 = -1, param_2 = -1, param_3 = -1,
-#                   desc_len = 0, desc_cap = 0, desc_pun = 0, desc_dig = 0)) %T>%
+#          wday = wday(activation_date)) %>% 
+#   select(-item_id, -image, -title, -description, -activation_date) %>% 
+#   replace_na(list(image_top_1 = -1, price = -1, 
+#                   param_1 = 0, param_2 = 0, param_3 = 0, 
+#                   desc_len = 0, desc_cap = 0, desc_pun = 0, 
+#                   desc_dig = 0, desc_capE = 0, desc_capR = 0)) %T>% 
 #   glimpse()
-
-# img ---------------------------------------------------------------------
-
+# 
+# rm(tr, te); gc()
+# 
+# #---------------------------
+# cat("Parsing text...\n")
+# it <- tr_te %$%
+#   str_to_lower(txt) %>%
+#   str_replace_all("[^[:alpha:]]", " ") %>%
+#   str_replace_all("\\s+", " ") %>%
+#   tokenize_word_stems(language = "russian") %>% 
+#   itoken()
+# 
+# vect <- create_vocabulary(it, ngram = c(1, 1), stopwords = stopwords("ru")) %>%
+#   prune_vocabulary(term_count_min = 3, doc_proportion_max = 0.4, vocab_term_max = 12500) %>% 
+#   vocab_vectorizer()
+# 
+# m_tfidf <- TfIdf$new(norm = "l2", sublinear_tf = T)
+# tfidf <-  create_dtm(it, vect) %>% 
+#   fit_transform(m_tfidf)
+# 
+# rm(it, vect, m_tfidf); gc()
+# 
+# #---------------------------
+# cat("Preparing data...\n")
+# X <- tr_te %>% 
+#   select(-txt) %>% 
+#   sparse.model.matrix(~ . - 1, .) %>% 
+#   cbind(tfidf)
+# 
+# rm(tr_te, tfidf); gc()
+# 
+# dtest <- xgb.DMatrix(data = X[-tri, ])
+# X <- X[tri, ]; gc()
+# tri <- caret::createDataPartition(y, p = 0.9, list = F) %>% c()
+# dtrain <- xgb.DMatrix(data = X[tri, ], label = y[tri])
+# dval <- xgb.DMatrix(data = X[-tri, ], label = y[-tri])
+# cols <- colnames(X)
+# 
+# rm(X, y, tri); gc()
+# 
+# #---------------------------
+# cat("Training model...\n")
+# p <- list(objective = "reg:logistic",
+#           booster = "gbtree",
+#           eval_metric = "rmse",
+#           nthread = 8,
+#           eta = 0.05,
+#           max_depth = 18,
+#           min_child_weight = 11,
+#           gamma = 0,
+#           subsample = 0.8,
+#           colsample_bytree = 0.7,
+#           alpha = 2.25,
+#           lambda = 0,
+#           nrounds = 5000)
+# 
+# m_xgb <- xgb.train(p, dtrain, p$nrounds, list(val = dval), print_every_n = 10, early_stopping_rounds = 50)
+# 
+# xgb.importance(cols, model = m_xgb) %>%   
+#   xgb.plot.importance(top_n = 35)
+# 
+# #---------------------------
+# cat("Creating submission file...\n")
+# read_csv("../input/sample_submission.csv")  %>%  
+#   mutate(deal_probability = predict(m_xgb, dtest)) %>%
+#   write_csv(paste0("xgb_tfidf", m_xgb$best_score, ".csv"))
