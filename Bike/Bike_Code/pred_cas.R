a750e9f111e9552fdb5d110bf2a18385f0c9dcde
LegenDad <hj.jedonald@gmail.com>
2018-06-17 03:15:01 +0900

    h_div

--- Bike/Bike_Code/pred_cas.R
+++ Bike/Bike_Code/pred_cas.R
@@ -1,17 +1,21 @@
 library(tidyverse)
 library(lubridate)
+library(skimr)
 
 btr <- read_csv("../input/train.csv")
 bte <- read_csv("../input/test.csv")
 
 glimpse(btr)
 glimpse(bte)
+btr %>% select(registered, casual) %>% skim()
 tri <- 1:nrow(btr)
 y <- log1p(btr$count)
 
 colnames(btr)
 colnames(bte)
 # -casual -registered -count
+cas <- btr$casual
+reg <- btr$registered
 
 bike <- btr %>% select(-casual, -registered, -count) %>% 
   bind_rows(bte) %>% 
@@ -26,27 +30,83 @@ bike <- btr %>% select(-casual, -registered, -count) %>%
          am = am(datetime) %>% as.integer(), 
          pm = pm(datetime) %>% as.integer()) %>% select(-datetime)
 
-bike <- bike %>% mutate(h_dvi = ifelse(hour <=8, 1, 
-                                       ifelse(hour ==9, 2, 
-                                              ifelse(hour >= 10, 3, 4))))
 tr <- bike[tri, ]
+te <- bike[-tri,]
 tr_index <- sample(nrow(tr)*0.7)  
 val_index <- sample(nrow(tr[tr_index,])*0.9)
 
 library(xgboost)
+
+cas_tr <- xgb.DMatrix(data = data.matrix(tr[tr_index,][val_index,]),
+                      label = cas[tr_index][val_index])
+
+cas_val <- xgb.DMatrix(data = data.matrix(tr[tr_index,][-val_index,]),
+                       label = cas[tr_index][-val_index])
+
+reg_tr <- xgb.DMatrix(data = data.matrix(tr[tr_index,][val_index,]),
+                      label = reg[tr_index][val_index])
+
+reg_val <- xgb.DMatrix(data = data.matrix(tr[tr_index,][-val_index,]),
+                       label = reg[tr_index][-val_index])
+
+cas_te <- xgb.DMatrix(data = data.matrix(tr[-tr_index,]))
+  
+p <- list(booster = "gbtree",
+          eval_metric = "rmse",
+          nthread = 8,
+          eta = 0.01,
+          max_depth = 18,
+          min_child_weight = 11,
+          gamma = 0,
+          subsample = 0.8,
+          colsample_bytree = 0.7,
+          alpha = 2.25,
+          lambda = 0,
+          nrounds = 5000)
+
+m_cas <- xgb.train(p, cas_tr, p$nrounds, list(val = cas_val), 
+                   print_every_n = 10, early_stopping_rounds = 200)
+m_reg <- xgb.train(p, reg_tr, p$nrounds, list(val = reg_val), 
+                   print_every_n = 10, early_stopping_rounds = 200)
+
+pred_cas <- predict(m_cas, cas_te)
+pred_rge <- predict(m_reg, cas_te)
+range(pred_cas); range(pred_rge)
+
+pred_cas <- ifelse(pred_cas < 0 , 0 , pred_cas)
+pred_rge <- ifelse(pred_rge < 0 , 0 , pred_rge)
+
+
+realtest <- xgb.DMatrix(data = data.matrix(te))
+
+pred_cas <- predict(m_cas, realtest)
+pred_rge <- predict(m_reg, realtest)
+range(pred_cas); range(pred_rge)
+
+pred_cas <- ifelse(pred_cas < 0 , 0 , pred_cas)
+pred_rge <- ifelse(pred_rge < 0 , 0 , pred_rge)
+
+tr$casual <- cas
+tr$registered <- reg
+
+te$casual <- pred_cas
+te$registered <- pred_rge
+
 
 dtest <- xgb.DMatrix(data = data.matrix(tr[-tr_index,]))
 
 dtrain <- xgb.DMatrix(data = data.matrix(tr[tr_index,][val_index,]),
                       label = y[tr_index][val_index])
 
-dval <- xgb.DMatrix(data = data.matrix(tr[tr_index,][-val_index,]), 
+dval <- xgb.DMatrix(data = data.matrix(tr[tr_index,][-val_index,]),
                     label = y[tr_index][-val_index])
+
+
 
 p <- list(booster = "gbtree",
           eval_metric = "rmse",
           nthread = 8,
-          eta = 0.05,
+          eta = 0.01,
           max_depth = 18,
           min_child_weight = 11,
           gamma = 0,
@@ -62,7 +122,8 @@ m_xgb <- xgb.train(p, dtrain, p$nrounds, list(val = dval),
 
 xgb.importance(feature_names = colnames(dtrain), m_xgb) %>% xgb.plot.importance()
 
-realtest <- xgb.DMatrix(data = data.matrix(bike[-tri,]))
+# realtest <- xgb.DMatrix(data = data.matrix(bike[-tri,]))
+realtest <- xgb.DMatrix(data = data.matrix(te))
 preT <- expm1(predict(m_xgb, realtest))
 range(preT)  
 sub1 = read.csv("../input/sampleSubmission.csv")
