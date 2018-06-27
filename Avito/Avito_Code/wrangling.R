796645a85fc4e11008d9fd4bf21c39595fb155c0
LegenDad <hj.jedonald@gmail.com>
2018-06-27 12:47:18 +0900

    wrangling

---
+++ Avito/Avito_Code/wrangling.R
@@ -0,0 +1,79 @@
+library(tidyverse)
+library(knitr)
+library(skimr)
+library(DT)
+library(lubridate)
+set.seed(0)
+avi <- read_csv("../input/train.csv")
+y <- avi$deal_probability
+
+avi <- avi %>% 
+  select(-deal_probability) %>% 
+  mutate(p1_na = is.na(param_1) %>% as.integer(), 
+         p2_na = is.na(param_2) %>% as.integer(), 
+         p3_na = is.na(param_3) %>% as.integer(), 
+         dsc_na = is.na(description) %>% as.integer(), 
+         img_na = is.na(image) %>% as.integer(), 
+         dsc_len = str_length(description), 
+         dsc_capE = str_count(description, "[A-Z]"), 
+         dsc_capR = str_count(description, "[А-Я]"), 
+         dsc_cap = str_count(description, "[A-ZА-Я]"), 
+         dsc_pun = str_count(description, "[[:punct:]]"), 
+         dsc_dig = str_count(description, "[[:digit:]]"), 
+         title_len = str_length(title), 
+         title_capE = str_count(title, "[A-Z]"), 
+         title_capR = str_count(title, "[А-Я]"), 
+         title_cap = str_count(title, "[A-ZА-Я]"), 
+         title_pun = str_count(title, "[[:punct:]]"), 
+         title_dig = str_count(title, "[[:digit:]]"), 
+         param_1 = factor(param_1) %>% as.integer(), 
+         param_2 = factor(param_2) %>% as.integer(), 
+         param_3 = factor(param_3) %>% fct_lump(prop=0.00005) %>% as.integer(), 
+         user_id = factor(user_id) %>% fct_lump(prop=0.00002) %>% as.integer(), 
+         city = factor(city) %>% fct_lump(prop=0.0003) %>% as.integer(), 
+         category_name = factor(category_name) %>% as.integer(), 
+         region = factor(region), 
+         parent_category_name = factor(parent_category_name), 
+         user_type = factor(user_type), 
+         mday = mday(activation_date) %>% as.factor(), 
+         wday = wday(activation_date) %>% as.factor(),
+         price = log1p(price), 
+         txt = paste(title, description, sep = " ")) %>% 
+  select(-item_id, -image, -title, -description, -activation_date) %>% 
+  replace_na(list(param_1 = 0, param_2 =0 , param_3 = 0, 
+                  dsc_len = 0, dsc_pun = 0, dsc_dig = 0, 
+                  dsc_capE = 0, dsc_capR = 0, dsc_cap = 0, 
+                  price = -1, image_top_1 = -1))
+  
+glimpse(avi)
+
+library(magrittr)
+library(tokenizers)
+library(text2vec)
+library(stopwords)
+library(Matrix)
+library(pryr)
+
+it <- avi %$% 
+  str_to_lower(txt) %>% 
+  str_replace_all("[^[:alpha:]]", " ") %>% 
+  str_replace_all("\\s+", " ") %>% 
+  tokenize_word_stems(language = "russian") %>% 
+  itoken()
+
+vect <- create_vocabulary(it, stopwords = stopwords("ru")) %>%  
+  prune_vocabulary(term_count_min = 3, doc_proportion_max = 0.4, 
+                   vocab_term_max = 12500) %>% 
+  vocab_vectorizer()
+
+m_tfidf <- TfIdf$new(norm = "l2", sublinear_tf = T)
+tfidf <- create_dtm(it, vect) %>% fit_transform(m_tfidf)
+
+mem_used()
+rm(it, vect, m_tfidf); gc()
+
+X <- avi %>% select(-txt) %>% sparse.model.matrix(~.-1, .) %>% cbind(tfidf)
+
+mem_used()
+rm(avi, tfidf); gc()
+
--- Bike/Bike_Code/ing2.R
+++ Bike/Bike_Code/ing2.R
@@ -70,27 +70,22 @@ params <- list(objective = "regression",
                bagging_fraction = .8, 
                bagging_freq = 5)
 
-# cas_train <- xgb.DMatrix(data = data.matrix(tr[index,]), 
-#                          label = cas[index])
-# cas_val <- xgb.DMatrix(data = data.matrix(tr[-index,]), 
-#                        label = cas[-index])
-# cas_xgb <- xgb.train(p, cas_train, p$nrounds, list(val = cas_val), 
-#                      print_every_n = 10, early_stopping_rounds = 200)
+cas_train <- xgb.DMatrix(data = tr[index,], label = cas[index])
+cas_val <- xgb.DMatrix(data = tr[-index,], label = cas[-index])
+cas_xgb <- xgb.train(p, cas_train, p$nrounds, list(val = cas_val),
+                     print_every_n = 10, early_stopping_rounds = 200)
 
-cas_lgtr <- lgb.Dataset(data = data.matrix(tr[index,]), 
-                        label = cas[index])
-
-cas_lgval <- lgb.Dataset(data = data.matrix(tr[-index,]), 
-                         label = cas[-index])
+cas_lgtr <- lgb.Dataset(data = tr[index,], label = cas[index])
+cas_lgval <- lgb.Dataset(data = tr[-index,], label = cas[-index])
 cas_lgb <- lgb.train(params, cas_lgtr, 
                      valids = list(train = cas_lgtr, validation = cas_lgval),
                      nthread = 8, nrounds = 5000, verbose = 1, 
                      early_stopping_rounds = 200, eval_freq = 50)
 
-# cas_test <- xgb.DMatrix(data = data.matrix(bike[-tri,]))
-# cas_pred <- predict(cas_xgb, cas_test)
+cas_test <- xgb.DMatrix(data = bike[-tri,])
+cas_pred <- predict(cas_xgb, cas_test)
 
-cas_lgte <- data.matrix(te)
+cas_lgte <- te
 cas_lgpred <- predict(cas_lgb, cas_lgte, n=cas_lgb$best_iter)
 
 range(cas)
@@ -100,29 +95,24 @@ cas_pred <- ifelse(cas_pred<0 , 0 , cas_pred)
 cas_lgpred <- ifelse(cas_lgpred<0 , 0 , cas_lgpred)
 
 tr <- tr %>% cbind(casual = cas)
-# te <- te %>% cbind(casual = (cas_pred+cas_lgpred) / 2)
-te <- te %>% cbind(casual = cas_lgpred)
+te <- te %>% cbind(casual = (cas_pred+cas_lgpred) / 2)
 
-# reg_xgtr <- xgb.DMatrix(data = data.matrix(tr[index,]), 
-#                         label = reg[index])
-# reg_xgval <- xgb.DMatrix(data = data.matrix(tr[-index,]), 
-#                          label = reg[-index])
-# reg_xgb <- xgb.train(p, reg_xgtr, p$nrounds, list(val = reg_xgval), 
-#                      print_every_n = 10, early_stopping_rounds = 200)
+reg_xgtr <- xgb.DMatrix(data = tr[index,], label = reg[index])
+reg_xgval <- xgb.DMatrix(data = tr[-index,], label = reg[-index])
+reg_xgb <- xgb.train(p, reg_xgtr, p$nrounds, list(val = reg_xgval),
+                     print_every_n = 10, early_stopping_rounds = 200)
 
-reg_lgtr <- lgb.Dataset(data = data.matrix(tr[index,]), 
-                        label = reg[index])
-reg_lgval <- lgb.Dataset(data = data.matrix(tr[-index,]), 
-                         label = reg[-index])
+reg_lgtr <- lgb.Dataset(data = tr[index,], label = reg[index])
+reg_lgval <- lgb.Dataset(data = tr[-index,], label = reg[-index])
 reg_lgb <- lgb.train(params, reg_lgtr, 
                      valids = list(train = reg_lgtr, validation = reg_lgval),
                      nthread = 8, nrounds = 5000, verbose = 1, 
                      early_stopping_rounds = 200, eval_freq = 50)
 
-# reg_xgte <- xgb.DMatrix(data = data.matrix(te))
-# reg_xgpred <- predict(reg_xgb, reg_xgte)
+reg_xgte <- xgb.DMatrix(data = te)
+reg_xgpred <- predict(reg_xgb, reg_xgte)
 
-reg_lgte <- data.matrix(te)
+reg_lgte <- te
 reg_lgpred <- predict(reg_lgb, reg_lgte, n=reg_lgb$best_iter)
 
 range(reg)
@@ -132,31 +122,27 @@ reg_xgpred <- ifelse(reg_xgpred<0 , 0 , reg_xgpred)
 reg_lgpred <- ifelse(reg_lgpred<0 , 0 , reg_lgpred)
 
 tr <- tr %>% cbind(registered = reg)
-# te <- te %>% cbind(registered = (reg_xgpred+reg_lgpred) / 2)
-te <- te %>% cbind(registered = reg_lgpred)
+te <- te %>% cbind(registered = (reg_xgpred+reg_lgpred) / 2)
 
-train <- xgb.DMatrix(data = data.matrix(tr[index,]), 
-                     label = y[index])
-val <- xgb.DMatrix(data = data.matrix(tr[-index,]), 
-                   label = y[-index])
+
+train <- xgb.DMatrix(data = tr[index,], label = y[index])
+val <- xgb.DMatrix(data = tr[-index,], label = y[-index])
 f_xgb <- xgb.train(p, train, p$nrounds, list(val = val), 
                    print_every_n = 1000, early_stopping_rounds = 200)
 
 xgb.importance(feature_names = colnames(train), f_xgb) %>% 
   xgb.plot.importance(top_n = 35)
 
-realtest <- xgb.DMatrix(data = data.matrix(te))
+realtest <- xgb.DMatrix(data = te)
 sub1 = read.csv("../input/sampleSubmission.csv")
 pred <- expm1(predict(f_xgb, realtest))
 range(pred)
 sub1$count <- pred
 write_csv(sub1, paste0("../SubM/xgb_", round(f_xgb$best_score, 5), ".csv"))
 
-dtrain <- lgb.Dataset(data = data.matrix(tr[index,]), 
-                      label = y[index])
-dval <- lgb.Dataset(data = data.matrix(tr[-index,]), 
-                    label = y [-index])
-realtest <- data.matrix(te)
+dtrain <- lgb.Dataset(data = tr[index,], label = y[index])
+dval <- lgb.Dataset(data = tr[-index,], label = y [-index])
+realtest <- te
 lgb_m <- lgb.train(params, dtrain, 
                    valids = list(train = dtrain, validation = dval),
                    nthread = 8, nrounds = 5000, verbose = 1, 
